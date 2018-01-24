53e74d0cd824ced528cc843b9c2b174015e277e3
chanxe <cH@mm@ch3r>
2018-01-23 23:08:59 -0600

    first commit

---
+++ src/Sym.java
@@ -0,0 +1,13 @@
+public class Sym {
+    //Globals
+    String t = "";
+    public Sym(String type){
+        t = type;
+    }
+    public String getType(){
+        return t;
+    }
+    public String toString(){
+        return t;
+    }
+}
---
+++ src/SymTable.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class SymTable {
+    public SymTable(){
+
+    }
+    public void addDec1(String name, Sym sym){
+
+    }
+    public void addScope(){
+
+    }
+    public Sym lookupLocal(String name){
+        return null;
+    }
+    public Sym lookupGlobal(String name){
+        return null;
+    }
+    public void removeScope(){
+
+    }
+    public void print(){
+
+    }
+}
